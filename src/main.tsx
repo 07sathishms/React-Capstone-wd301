1e9a4416468937769b1de7195b2dd37c7f57c248
07sathishms <91666613+07sathishms@users.noreply.github.com>
2023-10-23 19:50:35 +0530

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.js";
+import "./index.css";
+import { ThemeProvider } from "./context/theme.js";
+import { UserProvider } from "./context/user.js";
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+     <ThemeProvider>
+      <UserProvider>
+        <>
+          <App />
+        </>
+      </UserProvider>
+      </ThemeProvider>
+  </React.StrictMode>
+);
