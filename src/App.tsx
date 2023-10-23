7067e9283051852e1bb5496b1965ec75ad9f6d84
07sathishms <91666613+07sathishms@users.noreply.github.com>
2023-10-23 19:45:07 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
+import React, { useContext, useEffect } from "react";
+import { ThemeContext } from "./context/theme";
+function App() {
+  const currentTheme = useContext(ThemeContext);
+  useEffect(() => {
+    if (currentTheme.theme === "dark") {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  }, [currentTheme]);
+  return (
+    <div
+      className={`h-screen overflow-y-scroll no-scrollbar w-full mx-auto ${
+        currentTheme.theme === "dark" ? "dark" : ""
+      }`}
+    >
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
